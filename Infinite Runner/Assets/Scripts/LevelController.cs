e4d3566fa39e503185b7916dbd2019d12845631b
SlenderFox <36467674+SlenderFox@users.noreply.github.com>
2018-09-14 15:18:48 +1000

    -Fixed the LevelController -Working on platforms and implementing the platforms Brent made

--- Infinite Runner/Assets/Scripts/LevelController.cs
+++ Infinite Runner/Assets/Scripts/LevelController.cs
@@ -10,10 +10,10 @@ public class LevelController : MonoBehaviour
     public ScoreController sController;
 
     // Randomly generated number used for platform spawns
-    private int randomNo;
+    public int randomNo;
 
     // Difficulty settings
-    private int diffiulty = 0;
+    public uint diffiulty = 0;
 
     // Playform movement speed
     public float platformMoveSpeed = 100.0f;
@@ -21,21 +21,30 @@ public class LevelController : MonoBehaviour
     // Reference to the platform prefabs
     public GameObject platform_Empty;
     public GameObject platform_Test;
+    // Difficulty 0
     public GameObject platform_D0_1;
     public GameObject platform_D0_2;
     public GameObject platform_D0_3;
     public GameObject platform_D0_4;
     public GameObject platform_D0_5;
-    public GameObject platform_D0_6;
-    public GameObject platform_D0_7;
-    public GameObject platform_D0_8;
-    public GameObject platform_D0_9;
-    public GameObject platform_D0_10;
+    // Difficulty 1
+    public GameObject platform_D1_1;
+    public GameObject platform_D1_2;
+    public GameObject platform_D1_3;
+    public GameObject platform_D1_4;
+    public GameObject platform_D1_5;
+    // Difficulty 2
     public GameObject platform_D2_1;
+    public GameObject platform_D2_2;
+    public GameObject platform_D2_3;
+    public GameObject platform_D2_4;
+    public GameObject platform_D2_5;
+    // Difficulty 3
     public GameObject platform_D3_1;
     public GameObject platform_D3_2;
     public GameObject platform_D3_3;
     public GameObject platform_D3_4;
+    public GameObject platform_D3_5;
 
     //enum Platforms
     //{
@@ -65,38 +74,24 @@ public class LevelController : MonoBehaviour
 
     private void Awake()
     {
-        // Adds the starting platforms to the platform list
-        platforms.AddRange(GameObject.FindGameObjectsWithTag("LevelStart"));
-
         // Finds the scipts
         pController = FindObjectOfType<PlayerController>();
         dController = FindObjectOfType<DeathController>();
         sController = FindObjectOfType<ScoreController>();
+
+        // Adds the starting platforms to the platform list
+        platforms.AddRange(GameObject.FindGameObjectsWithTag("LevelStart"));
     }
 
     private void FixedUpdate()
     {
         // Controls difficulty based on score (distance travelled)
-        if (sController.Score < 200)
-        {
-            if (diffiulty != 0)
-                diffiulty = 0;
-        }
-        else if (sController.Score >= 200 && sController.Score < 600)
-        {
-            if (diffiulty != 1)
-                diffiulty = 1;
-        }
-        else if (sController.Score >= 600 && sController.Score < 1000)
-        {
-            if (diffiulty != 2)
-                diffiulty = 2;
-        }
-        else
-        {
-            if (diffiulty != 3)
-                diffiulty = 3;
-        }
+        if (sController.Score > 800 && diffiulty == 0)
+            diffiulty = 1;
+        else if (sController.Score > 2400 && diffiulty == 1)
+            diffiulty = 2;
+        else if (sController.Score > 5000 && diffiulty == 2)
+            diffiulty = 3;
 
         // Resets the newPlatforms and deletedPlatforms list
         newPlatforms.Clear();
@@ -109,88 +104,128 @@ public class LevelController : MonoBehaviour
             if (platform.transform.position.z < -100)
             {
                 // Randomizes the platforms then accounts for difficulty
-                randomNo = Random.Range(0, 20);
+                randomNo = Random.Range(0, 9);
                 //Debug.Log(randomNo);
-                if (randomNo >= 0 && randomNo < 6)
+                if (randomNo >= 0 && randomNo < 4)
                 {
                     GameObject newPlatform = Instantiate(platform_Empty, new Vector3(0, -0.5f, platform.transform.position.z + 1200.0f), Quaternion.identity);
                     newPlatforms.Add(newPlatform);
                 }
-                else if (randomNo >= 6 && randomNo < 7)
+                else
                 {
-                    GameObject newPlatform = Instantiate(platform_D0_1, new Vector3(0, -0.5f, platform.transform.position.z + 1200.0f), Quaternion.identity);
-                    newPlatforms.Add(newPlatform);
+                    switch (diffiulty)
+                    {
+                        case 0:
+                            if (randomNo == 4)
+                            {
+                                GameObject newPlatform = Instantiate(platform_D0_1, new Vector3(0, -0.5f, platform.transform.position.z + 1200.0f), Quaternion.identity);
+                                newPlatforms.Add(newPlatform);
+                            }
+                            else if (randomNo == 5)
+                            {
+                                GameObject newPlatform = Instantiate(platform_D0_2, new Vector3(0, -0.5f, platform.transform.position.z + 1200.0f), Quaternion.identity);
+                                newPlatforms.Add(newPlatform);
+                            }
+                            else if (randomNo == 6)
+                            {
+                                GameObject newPlatform = Instantiate(platform_D0_3, new Vector3(0, -0.5f, platform.transform.position.z + 1200.0f), Quaternion.identity);
+                                newPlatforms.Add(newPlatform);
+                            }
+                            else if (randomNo == 7)
+                            {
+                                GameObject newPlatform = Instantiate(platform_D0_4, new Vector3(0, -0.5f, platform.transform.position.z + 1200.0f), Quaternion.identity);
+                                newPlatforms.Add(newPlatform);
+                            }
+                            else if (randomNo == 8)
+                            {
+                                GameObject newPlatform = Instantiate(platform_D0_5, new Vector3(0, -0.5f, platform.transform.position.z + 1200.0f), Quaternion.identity);
+                                newPlatforms.Add(newPlatform);
+                            }
+                            break;
+                        case 1:
+                            if (randomNo == 4)
+                            {
+                                GameObject newPlatform = Instantiate(platform_D1_1, new Vector3(0, -0.5f, platform.transform.position.z + 1200.0f), Quaternion.identity);
+                                newPlatforms.Add(newPlatform);
+                            }
+                            else if (randomNo == 5)
+                            {
+                                GameObject newPlatform = Instantiate(platform_D1_2, new Vector3(0, -0.5f, platform.transform.position.z + 1200.0f), Quaternion.identity);
+                                newPlatforms.Add(newPlatform);
+                            }
+                            else if (randomNo == 6)
+                            {
+                                GameObject newPlatform = Instantiate(platform_D1_3, new Vector3(0, -0.5f, platform.transform.position.z + 1200.0f), Quaternion.identity);
+                                newPlatforms.Add(newPlatform);
+                            }
+                            else if (randomNo == 7)
+                            {
+                                GameObject newPlatform = Instantiate(platform_D1_4, new Vector3(0, -0.5f, platform.transform.position.z + 1200.0f), Quaternion.identity);
+                                newPlatforms.Add(newPlatform);
+                            }
+                            else if (randomNo == 8)
+                            {
+                                GameObject newPlatform = Instantiate(platform_D1_5, new Vector3(0, -0.5f, platform.transform.position.z + 1200.0f), Quaternion.identity);
+                                newPlatforms.Add(newPlatform);
+                            }
+                            break;
+                        case 2:
+                            if (randomNo == 4)
+                            {
+                                GameObject newPlatform = Instantiate(platform_D2_1, new Vector3(0, -0.5f, platform.transform.position.z + 1200.0f), Quaternion.identity);
+                                newPlatforms.Add(newPlatform);
+                            }
+                            else if (randomNo == 5)
+                            {
+                                GameObject newPlatform = Instantiate(platform_D2_2, new Vector3(0, -0.5f, platform.transform.position.z + 1200.0f), Quaternion.identity);
+                                newPlatforms.Add(newPlatform);
+                            }
+                            else if (randomNo == 6)
+                            {
+                                GameObject newPlatform = Instantiate(platform_D2_3, new Vector3(0, -0.5f, platform.transform.position.z + 1200.0f), Quaternion.identity);
+                                newPlatforms.Add(newPlatform);
+                            }
+                            else if (randomNo == 7)
+                            {
+                                GameObject newPlatform = Instantiate(platform_D2_4, new Vector3(0, -0.5f, platform.transform.position.z + 1200.0f), Quaternion.identity);
+                                newPlatforms.Add(newPlatform);
+                            }
+                            else if (randomNo == 8)
+                            {
+                                GameObject newPlatform = Instantiate(platform_D2_5, new Vector3(0, -0.5f, platform.transform.position.z + 1200.0f), Quaternion.identity);
+                                newPlatforms.Add(newPlatform);
+                            }
+                            break;
+                        case 3:
+                            if (randomNo == 4)
+                            {
+                                GameObject newPlatform = Instantiate(platform_D3_1, new Vector3(0, -0.5f, platform.transform.position.z + 1200.0f), Quaternion.identity);
+                                newPlatforms.Add(newPlatform);
+                            }
+                            else if (randomNo == 5)
+                            {
+                                GameObject newPlatform = Instantiate(platform_D3_2, new Vector3(0, -0.5f, platform.transform.position.z + 1200.0f), Quaternion.identity);
+                                newPlatforms.Add(newPlatform);
+                            }
+                            else if (randomNo == 6)
+                            {
+                                GameObject newPlatform = Instantiate(platform_D3_3, new Vector3(0, -0.5f, platform.transform.position.z + 1200.0f), Quaternion.identity);
+                                newPlatforms.Add(newPlatform);
+                            }
+                            else if (randomNo == 7)
+                            {
+                                GameObject newPlatform = Instantiate(platform_D3_4, new Vector3(0, -0.5f, platform.transform.position.z + 1200.0f), Quaternion.identity);
+                                newPlatforms.Add(newPlatform);
+                            }
+                            else if (randomNo == 8)
+                            {
+                                GameObject newPlatform = Instantiate(platform_D3_5, new Vector3(0, -0.5f, platform.transform.position.z + 1200.0f), Quaternion.identity);
+                                newPlatforms.Add(newPlatform);
+                            }
+                            break;
+                    }
                 }
-                else if (randomNo >= 7 && randomNo < 8)
-                {
-                    GameObject newPlatform = Instantiate(platform_D0_2, new Vector3(0, -0.5f, platform.transform.position.z + 1200.0f), Quaternion.identity);
-                    newPlatforms.Add(newPlatform);
-                }
-                else if (randomNo >= 8 && randomNo < 9)
-                {
-                    GameObject newPlatform = Instantiate(platform_D0_3, new Vector3(0, -0.5f, platform.transform.position.z + 1200.0f), Quaternion.identity);
-                    newPlatforms.Add(newPlatform);
-                }
-                else if (randomNo >= 9 && randomNo < 10)
-                {
-                    GameObject newPlatform = Instantiate(platform_D0_4, new Vector3(0, -0.5f, platform.transform.position.z + 1200.0f), Quaternion.identity);
-                    newPlatforms.Add(newPlatform);
-                }
-                else if (randomNo >= 10 && randomNo < 11)
-                {
-                    GameObject newPlatform = Instantiate(platform_D0_5, new Vector3(0, -0.5f, platform.transform.position.z + 1200.0f), Quaternion.identity);
-                    newPlatforms.Add(newPlatform);
-                }
-                else if (randomNo >= 11 && randomNo < 12)
-                {
-                    GameObject newPlatform = Instantiate(platform_D0_6, new Vector3(0, -0.5f, platform.transform.position.z + 1200.0f), Quaternion.identity);
-                    newPlatforms.Add(newPlatform);
-                }
-                else if (randomNo >= 12 && randomNo < 13)
-                {
-                    GameObject newPlatform = Instantiate(platform_D0_7, new Vector3(0, -0.5f, platform.transform.position.z + 1200.0f), Quaternion.identity);
-                    newPlatforms.Add(newPlatform);
-                }
-                else if (randomNo >= 13 && randomNo < 14)
-                {
-                    GameObject newPlatform = Instantiate(platform_D0_8, new Vector3(0, -0.5f, platform.transform.position.z + 1200.0f), Quaternion.identity);
-                    newPlatforms.Add(newPlatform);
-                }
-                else if (randomNo >= 14 && randomNo < 15)
-                {
-                    GameObject newPlatform = Instantiate(platform_D0_9, new Vector3(0, -0.5f, platform.transform.position.z + 1200.0f), Quaternion.identity);
-                    newPlatforms.Add(newPlatform);
-                }
-                else if (randomNo >= 15 && randomNo < 16)
-                {
-                    GameObject newPlatform = Instantiate(platform_D0_10, new Vector3(0, -0.5f, platform.transform.position.z + 1200.0f), Quaternion.identity);
-                    newPlatforms.Add(newPlatform);
-                }
-                else if (randomNo >= 16 && randomNo < 17)
-                {
-                    GameObject newPlatform = Instantiate(platform_D2_1, new Vector3(0, -0.5f, platform.transform.position.z + 1200.0f), Quaternion.identity);
-                    newPlatforms.Add(newPlatform);
-                }
-                else if (randomNo >= 17 && randomNo < 18)
-                {
-                    GameObject newPlatform = Instantiate(platform_D3_1, new Vector3(0, -0.5f, platform.transform.position.z + 1200.0f), Quaternion.identity);
-                    newPlatforms.Add(newPlatform);
-                }
-                else if (randomNo >= 18 && randomNo < 19)
-                {
-                    GameObject newPlatform = Instantiate(platform_D3_2, new Vector3(0, -0.5f, platform.transform.position.z + 1200.0f), Quaternion.identity);
-                    newPlatforms.Add(newPlatform);
-                }
-                else if (randomNo >= 19 && randomNo < 20)
-                {
-                    GameObject newPlatform = Instantiate(platform_D3_3, new Vector3(0, -0.5f, platform.transform.position.z + 1200.0f), Quaternion.identity);
-                    newPlatforms.Add(newPlatform);
-                }
-                else if (randomNo == 20)
-                {
-                    GameObject newPlatform = Instantiate(platform_D3_4, new Vector3(0, -0.5f, platform.transform.position.z + 1200.0f), Quaternion.identity);
-                    newPlatforms.Add(newPlatform);
-                }
+                // Removes the platform behind the player
                 Destroy(platform);
                 deletedPlatforms.Add(platform);
             }
